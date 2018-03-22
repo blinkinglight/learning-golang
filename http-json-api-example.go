b235daaf27a0bb848dd78e09ff9cf3ed2cd5a44a
Mindaugas Žvirblis <tinklapiai@gmail.com>
2018-03-22 21:02:31 +0200

    ...

--- http-json-api-example.go
+++ http-json-api-example.go
@@ -2,17 +2,23 @@ package main
 
 import (
 	"encoding/json"
+	"flag"
 	"fmt"
 	"github.com/asaskevich/govalidator"
 	"github.com/gorilla/mux"
 	"log"
 	"net/http"
+	"strings"
 )
+
+const apiPrefix = "api"
+const apiVersion = "v1"
 
 type Response struct {
 	Errors  map[string]string `json:"errors"`
 	Message string            `json:"message"`
 	Success bool              `json:"success"`
+	Data    interface{}       `json:"data"`
 }
 
 type Post struct {
@@ -20,22 +26,32 @@ type Post struct {
 	Message string `valid:"required,ascii" json:"message"`
 }
 
+var bindTo = flag.String("bind-to", ":9090", "-bind-to :port or host:port")
+
 func main() {
 
+	flag.Parse()
+
 	r := mux.NewRouter()
-	pr := r.PathPrefix("/v1").Methods("POST", "PUT", "DELETE").Subrouter()
-	gr := r.PathPrefix("/v1").Methods("GET").Subrouter()
+	pr := r.PathPrefix(fmt.Sprintf("/%s/%s", apiPrefix, apiVersion)).Methods("POST", "PUT").Subrouter()
+	dr := r.PathPrefix(fmt.Sprintf("/%s/%s", apiPrefix, apiVersion)).Methods("DELETE").Subrouter()
+	gr := r.PathPrefix(fmt.Sprintf("/%s/%s", apiPrefix, apiVersion)).Methods("GET").Subrouter()
 
-	_ = gr
-
+	// api post routes
 	pr.HandleFunc("/posts", hPostPosts)
+	// api delete routes
+	dr.HandleFunc("/post/{id:[0-9]+}", hDeletePost)
+	// api get routes
+	gr.HandleFunc("/posts", hGetPosts)
 
-	log.Fatal(http.ListenAndServe(":9090", wrapper(r)))
+	// public methods
+	r.HandleFunc("/public", publicMethod)
+
+	log.Fatal(http.ListenAndServe(*bindTo, wrapper(r)))
 }
 
 func wrapper(h http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-
 		defer func() {
 			if rec := recover(); rec != nil {
 				log.Printf("panic: %v", r.URL.String())
@@ -51,7 +67,7 @@ func wrapper(h http.Handler) http.Handler {
 	})
 }
 
-func jsonResponse(w http.ResponseWriter, success bool, errors error) {
+func jsonResponse(w http.ResponseWriter, success bool, errors error, data interface{}) {
 	var response Response
 
 	if errors == nil {
@@ -61,13 +77,19 @@ func jsonResponse(w http.ResponseWriter, success bool, errors error) {
 	response.Errors = govalidator.ErrorsByField(errors)
 	response.Message = errors.Error()
 	response.Success = success
+	response.Data = data
 
 	b, _ := json.Marshal(response)
 
+	w.Header().Set("Content-Type", "application/json")
 	w.Write(b)
 }
 
 func checkToken(w http.ResponseWriter, r *http.Request) bool {
+	if !strings.HasPrefix(r.URL.String(), "/"+apiPrefix) {
+		return true
+	}
+
 	token := r.URL.Query().Get("token")
 
 	if r.Header.Get("X-Access-Token") != "" {
@@ -75,12 +97,21 @@ func checkToken(w http.ResponseWriter, r *http.Request) bool {
 	}
 
 	//TODO: implement
-	if token == "morningstar" {
+	if isValidToken(token) {
 		return true
 	}
 
-	http.Error(w, "Unauthorized", http.StatusUnauthorized)
+	w.Header().Set("Content-Type", "application/json")
+	http.Error(w, `"Unauthorized"`, http.StatusUnauthorized)
 
+	return false
+}
+
+// your token check method here
+func isValidToken(token string) bool {
+	if token == "morningstar" {
+		return true
+	}
 	return false
 }
 
@@ -95,14 +126,14 @@ func hPostPosts(w http.ResponseWriter, r *http.Request) {
 	err := json.NewDecoder(r.Body).Decode(&post)
 
 	if err != nil {
-		jsonResponse(w, false, err)
+		jsonResponse(w, false, err, nil)
 		return
 	}
 
 	isValid, err := govalidator.ValidateStruct(post)
 
 	if !isValid {
-		jsonResponse(w, false, err)
+		jsonResponse(w, false, err, nil)
 		return
 	}
 
@@ -110,9 +141,36 @@ func hPostPosts(w http.ResponseWriter, r *http.Request) {
 	// save to database
 	// send to mq something
 	// etc.
-	jsonResponse(w, true, nil)
+	jsonResponse(w, true, nil, nil)
 	return
 
+}
+
+// example get posts
+type Record struct {
+	Title string
+	Body  string
+}
+
+type Records []*Record
+
+func hGetPosts(w http.ResponseWriter, r *http.Request) {
+	records := &Records{
+		&Record{"title 1", "long body 1"},
+		&Record{"title 2", "long body 2"},
+		&Record{"title 3", "long body 3"},
+	}
+	jsonResponse(w, true, nil, records)
+}
+
+// example delete post
+func hDeletePost(w http.ResponseWriter, r *http.Request) {
+	varz := mux.Vars(r)
+	jsonResponse(w, true, nil, fmt.Sprintf("Deleting record with id: %v", varz["id"]))
+}
+
+func publicMethod(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Public method\n"))
 }
 
 // MIT License
