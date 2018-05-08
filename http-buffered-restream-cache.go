1dfcab10aa833606afc9b480fa0439ea4dbd3c0f
Mindaugas Žvirblis <tinklapiai@gmail.com>
2018-05-08 22:46:36 +0300

    it works. i think.

--- http-buffered-restream-cache.go
+++ http-buffered-restream-cache.go
@@ -1,128 +1,122 @@
 package main
 
 import (
-	"fmt"
 	"io"
 	"net/http"
 	"runtime"
 	"sync"
+	"sync/atomic"
 	"time"
 )
 
-type Cache struct {
-	Cache []byte
-	Pos   int
-	Eof   bool
-	lock  sync.RWMutex
-}
+func main() {
 
-func (c *Cache) Read(r io.Reader) {
-	pos := 0
-	rl := 2 << 16
-	data := []byte{}
-	for {
-		if c.Pos <= pos && !c.Eof {
-			continue
-		}
-		if pos+rl < c.Pos {
-			c.lock.RLock()
-			data = c.Cache[pos : rl+pos]
-			c.lock.RUnlock()
-			pos = pos + rl
-			r.Read(data)
-		} else {
-			c.lock.RLock()
-			data = c.Cache[pos:len(c.Cache)]
-			c.lock.RUnlock()
-			pos = pos + (len(c.Cache) - pos)
-			r.Read(data)
-			if c.Eof {
+	t := New()
+
+	ht := http.Client{}
+	r, _ := ht.Get("http://mirror.vpsnet.com/centos/7/isos/x86_64/CentOS-7-x86_64-DVD-1708.iso")
+	go func() {
+		io.Copy(t, r.Body)
+		r.Body.Close()
+		t.Done()
+	}()
+
+	http.HandleFunc("/out", func(w http.ResponseWriter, r *http.Request) {
+		for b := range t.Read() {
+			_, e := w.Write(b)
+			if e != nil {
 				return
 			}
 		}
+	})
 
-	}
-}
-
-func (c *Cache) Write(b []byte) (int, error) {
-	c.lock.Lock()
-	c.Cache = append(c.Cache, b...)
-	c.lock.Unlock()
-	c.Pos = len(c.Cache) - 1
-
-	return len(b), nil
-}
-
-type Readeris struct {
-	num int
-	out io.Writer
-}
-
-func (r *Readeris) Read(b []byte) (int, error) {
-	r.out.Write(b)
-	return len(b), nil
-}
-
-type Writeris struct {
-	num int
-}
-
-func (w *Writeris) Write(b []byte) (int, error) {
-	fmt.Printf("%d %s\n", w.num, b)
-	return len(b), nil
-}
-
-var c *Cache
-var urls = make(map[string]*Cache)
-var lock sync.RWMutex
-
-func ht(w http.ResponseWriter, r *http.Request) {
-	readeris := new(Readeris)
-	readeris.num = 30
-	readeris.out = w
-	c.Read(readeris)
-}
-
-func rd(w http.ResponseWriter, r *http.Request) {
-	path := r.URL.Path
-	var ok bool
-	var cc *Cache
-	lock.Lock()
-	cc, ok = urls[r.URL.Path]
-	// lock.Unlock()
-	if !ok {
-		// lock.Lock()
-		cc = new(Cache)
-		cc.Cache = []byte{}
-		urls[path] = cc
-		lock.Unlock()
-		go func() {
-			url := "http://127.0.0.1" + path
-			fmt.Println("downloading: " + url)
-			rsp, _ := (&http.Client{}).Get(url)
-			defer rsp.Body.Close()
-			_, e := io.Copy(cc, rsp.Body)
-			if e != nil {
-				fmt.Printf("error: %v\n", e)
-			}
-			cc.Eof = true
-		}()
-	} else [
-		lock.Unlock()
-	]
-	readeris := new(Readeris)
-	readeris.num = 30
-	readeris.out = w
-	cc.Read(readeris)
-}
-
-func main() {
-
-	c = new(Cache)
-	c.Cache = []byte{}
-
-	http.HandleFunc("/", rd)
-	fmt.Println(http.ListenAndServe(":8080", nil))
+	http.ListenAndServe(":9002", nil)
 
 	runtime.Goexit()
+}
+
+func New() *T {
+	t := new(T)
+	t.d = [][]byte{}
+	t.dc = make(chan []byte)
+	t.mu = &sync.RWMutex{}
+	return t
+}
+
+type T struct {
+	d   [][]byte
+	cnt int32
+	dc  chan []byte
+	eof bool
+	mu  *sync.RWMutex
+}
+
+func (t *T) Done() {
+	t.eof = true
+}
+
+func (t *T) EOF() {
+	t.eof = true
+}
+
+func (t *T) Read() chan []byte {
+	ch := make(chan []byte)
+	var num int32 = 0
+	fln := func() int {
+		t.mu.RLock()
+		defer t.mu.RUnlock()
+		return len(t.d)
+	}
+	for fln() == 0 {
+		time.Sleep(10 * time.Millisecond)
+	}
+	fn := func() []byte {
+		t.mu.RLock()
+		defer t.mu.RUnlock()
+		return t.d[num]
+	}
+	go func() {
+		defer func() {
+			close(ch)
+		}()
+		for {
+			if t.eof == true {
+				pos := atomic.LoadInt32(&t.cnt)
+				for pos > num {
+					select {
+					case ch <- fn():
+					case <-time.After(time.Second):
+						return
+					}
+					num++
+				}
+			}
+			for t.eof == false {
+				pos := atomic.LoadInt32(&t.cnt)
+				for pos > num {
+					select {
+					case ch <- fn():
+					case <-time.After(time.Second):
+						return
+					}
+					num++
+				}
+			}
+			if t.eof == true && atomic.LoadInt32(&t.cnt) == num {
+				return
+			}
+			time.Sleep(10 * time.Millisecond)
+		}
+
+	}()
+	return ch
+}
+
+func (t *T) Write(chunk []byte) (n int, err error) {
+	t.mu.Lock()
+	t.d = append(t.d, chunk)
+	t.mu.Unlock()
+	atomic.AddInt32(&t.cnt, 1)
+
+	return len(chunk), nil
 }
